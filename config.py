a960374341c9c6bd3f2e00cc79998c6b3f5373ef
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2022-11-29 12:19:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 #Owner user id   
     AUTH  = os.environ.get("OWNER_ID", "5143506371")
 #Pyrogram string session using (https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1)
-    SESSION = os.environ.get("SESSION", "BQBR_9kJA0exmNqUbxMaqy972QYrn6-ltg_Pdt6DxSUskg2qHMn6gF-e3eIa_lGPQOTZU_lAaq4nZMARc39Oi_ISO_mkm_-Ldi2_ffdey9DKC-tUuzy4z2oBx5vkkdayLkf3E4aNemNt9LVxZLwGHWXv0b-L5J-oBDZQ-jCK-9xtpiMydRgEHYnDrVLjszMux7a7TEuat-kVQLLcnc4fPKFphkDxR7byMfJA-g-u8qmJ9_797Aaxck1JnAWqugB2L0Lu-ukDE91D1QAHdLpJIya4bX1LpkTJmdNUNo7xA-Pk9XdybmoaF_UOI6yxa-zUD0iJZTbgF6tIWBIvzzYuoqBhcdf4KQA")
+    SESSION = os.environ.get("SESSION", "AQA5aXqC5nSWIQHtNn0wDfBO1fz6pyxNHTEh2AHVSYPB_SzoEO0miF_4naTMruoVM3fnmX_QwmSSR6UK_jr-au4Z923JEp-6qlYDOz0m6vDzwTzX3paHRVAiOg5yBFDII9MlDeLKIAg-CJPttolByprAPL3RBej7YPL5Ap3iJztKIqNNBPD4sjmWKwMfFDhUikFr_Bb4iqLMb5cESSb6eY0c9xRIulIRmVKYvFJjD5-cODh9jvhXKRWxC-afvua9kCZDsQpuf8W-emwUQTSHgZq4MRwPTHdsHJio08d9bmZ_quxB8VYekdug-1yBP6vjppCfiZnw8y8celMewDvBWzR5AAAAATKTrcMA")
   
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
